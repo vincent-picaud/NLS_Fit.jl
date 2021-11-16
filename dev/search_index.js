cb2a953b0022f132cee30482d04cbb500330d10f
Documenter.jl <documenter@juliadocs.github.io>
2021-11-16 12:24:09 +0000

    build based on 07cd2c5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NLS_Fit","category":"page"},{"location":"#NLS_Fit","page":"Home","title":"NLS_Fit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NLS_Fit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NLS_Fit]","category":"page"}]
+}
