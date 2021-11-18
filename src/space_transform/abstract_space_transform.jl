0921a33606a4e02b4abba926e2eba348e77c6cc4
vincent-picaud <picaud.vincent@gmail.com>
2021-11-18 15:52:40 +0100

    add space transform dir

---
+++ src/space_transform/abstract_space_transform.jl
@@ -0,0 +1,34 @@
+@doc raw"""
+
+Space transformation base type. 
+
+This type is an abstraction of ``hat{X}↦X``.
+
+Such transformations are used for recalibration or parameters varying
+with the space variable ``X``.
+
+# Interface
+
+- parameter_size
+- eval_x
+
+"""
+abstract type Abstract_Space_Transform end
+
+@doc raw"""
+```julia
+parameter_size(::Abstract_Space_Transform) -> Int
+```
+
+Return θ parameter vector length
+"""
+parameter_size(::Abstract_Space_Transform) = @assert(false,"To implement")
+
+@doc raw"""
+```julia
+eval_x(m::Abstract_Space_Transform,X_hat::AbstractVector,θ::AbstractVector) -> X::AbstractVector
+```
+
+Compute ``X=X(\hat{X})``.
+"""
+eval_x(m::Abstract_Space_Transform,X_hat::AbstractVector,θ::AbstractVector) = @assert(false,"To implement")   
