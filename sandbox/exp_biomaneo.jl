f83c0906fa83889d7de64c2adbb91b6c0582c78c
vincent-picaud <picaud.vincent@gmail.com>
2021-11-26 16:18:44 +0100

    Dev: exp
    Seems ok... must add calibration

--- sandbox/exp_biomaneo.jl
+++ sandbox/exp_biomaneo.jl
@@ -33,7 +33,7 @@ end
 
 # Create a vector of models. Each model associated to one group
 #
-function NLS_Models.create_model(grouped::GroupedBySupport{IsotopicMotif})
+function create_vector_of_models(grouped::GroupedBySupport{IsotopicMotif})
     n_group = group_size(grouped)
     map(idx_group -> create_model(grouped,idx_group), 1:n_group)
 end 
@@ -49,27 +49,70 @@ end
 #
 # Attention: use functions from NLS_Models
 #
-function extract_ROI_spectrum(grouped::GroupedBySupport{IsotopicMotif},sp)
+function extract_ROI_spectrum_range_pair(grouped::GroupedBySupport{IsotopicMotif},sp::Spectrum)
     ROI_interval = group_support_as_interval(grouped)
     ROI_range = create_range_from_interval(ROI_interval,spectrum.X)
 
     Spectrum(extract_ROIs(ROI_range,sp.X),
-              extract_ROIs(ROI_range,sp.Y)),
+             extract_ROIs(ROI_range,sp.Y)),
     length.(ROI_range)
-    
 end 
 
+# ================================================================
 
+# create stacked model and associated ROI spectrum
+# 
+function create_stacked_model_ROI_spectrum_pair(grouped::GroupedBySupport{IsotopicMotif},sp_before_ROI::Spectrum)
+    vector_of_models =  create_vector_of_models(grouped)
+    roi_spectrum, roi_lengths = extract_ROI_spectrum_range_pair(grouped,sp_before_ROI)
+
+    Model2Fit_Stacked(vector_of_models,roi_lengths),roi_spectrum
+end
 
 # ****************************************************************
 
-# Perform fit 
-spectrum = read_spectrum_Biomaneo("/home/picaud/GitHub/NLS_Models.jl/data/0000000095.txt")
+# Perform fit
+# ================
+
+# Inputs
+#
+spectrum = read_spectrum_Biomaneo("/home/picaud/GitHub/NLS_Models.jl/data/0000000001.txt")
 spectrum.Y ./= maximum(spectrum.Y)
 vect_of_isotopicmotif = hardcoded_IsotopicMotifVect()
+
+# Remove baseline
+#
+Y_baseline = compute_baseline_snip(spectrum,
+                                   snip_halfwindow = 60,
+                                   smoothing_halfwindow = 20)
+
+spectrum = spectrum - Y_baseline
+
+# Create ROI model & spectrum
+#
 grouped = groupbysupport(vect_of_isotopicmotif,by=get_ROI_interval)
+stacked_models,ROI_spectrum = create_stacked_model_ROI_spectrum_pair(grouped,spectrum)
 
-create_model(grouped)
+# create θ : (h,σ) x number of ROIs
+#
+n_θ = NLS_Fit.parameter_size(stacked_models)
+θ_init = zeros(n_θ) .+ 0.5
+θ_lb = zeros(n_θ)
+θ_ub = zeros(n_θ) .+ 6
 
-extract_ROI_spectrum(grouped,spectrum)
+# prepare solver
+#
+bc = BoundConstraints(θ_lb,θ_ub)
+nls = NLS_ForwardDiff_From_Model2Fit(stacked_models,ROI_spectrum.X,ROI_spectrum.Y)
+conf = Levenberg_Marquardt_BC_Conf()
+
+result = NLS_Solver.solve(nls,θ_init,bc,conf)
+
+Y_fit = eval_y(stacked_models,ROI_spectrum.X,solution(result))
+
+# gnuplot
+#
+using DelimitedFiles
+
+writedlm("poub.txt",hcat(ROI_spectrum.X,ROI_spectrum.Y,Y_fit))
 
