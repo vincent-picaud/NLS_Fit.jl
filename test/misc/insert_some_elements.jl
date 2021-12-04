36f63a6896d4fbdafce0e9402f440436358bd2bd
vincent-picaud <picaud.vincent@gmail.com>
2021-12-04 08:47:02 +0100

    Extend insert_some_elements to support constant element
    This is done using an *iterable* element

--- test/misc/insert_some_elements.jl
+++ test/misc/insert_some_elements.jl
@@ -1,18 +1,38 @@
 @testset "insert_some_elements.jl" begin
-    using Random: seed!
-    using StatsBase: sample
-    
-    seed!(1234)
-
-    for i in 0:10
-        X=[1:10;]
-        indices = sort(sample(1:length(X),i,replace=false))
-        elements=X[indices]
+    @testset "elements = vector" begin
+        using Random: seed!
+        using StatsBase: sample
         
-        X_deflated = deleteat!(copy(X),indices)
-        X_recons = insert_some_elements(X_deflated,indices,elements)
+        seed!(1234)
 
-        @test X_recons == X
+        for i in 0:10
+            X=[1:10;]
+            indices = sort(sample(1:length(X),i,replace=false))
+            elements=X[indices]
+            
+            X_deflated = deleteat!(copy(X),indices)
+            X_recons = insert_some_elements(X_deflated,indices,elements)
+
+            @test X_recons == X
+        end
     end
 
+    @testset "elements = constant" begin
+        using Random: seed!
+        using StatsBase: sample
+        
+        seed!(1234)
+
+        for i in 0:10
+            X=[1:10;]
+            indices = sort(sample(1:length(X),i,replace=false))
+            element = 2021
+            X[indices] .= element
+            
+            X_deflated = deleteat!(copy(X),indices)
+            X_recons = insert_some_elements(X_deflated,indices,element)
+
+            @test X_recons == X
+        end
+    end
 end
