775c307f6447931a1fda01c5c650cdbfd5bdc6e6
vincent-picaud <picaud.vincent@gmail.com>
2022-01-03 15:52:56 +0100

    Code doc: abstract_map

--- src/map/abstract_map.jl
+++ src/map/abstract_map.jl
@@ -2,7 +2,8 @@
 
 Map base type. 
 
-This type is an abstraction of a ``f:X ↦ Y=f(X)``.
+This type is an abstraction of a ``f:X ↦ Y=f_{\hat{\theta}}(X)``. 
+Where ``\hat{\theta}`` is map parameter vector.
 
 Such transformations can be used in various contexts:
 - calibration task: we have a reference ``\hat{X}`` to a calibrated ``X``
@@ -21,15 +22,15 @@ abstract type Abstract_Map end
 parameter_size(::Abstract_Map) -> Int
 ```
 
-Return θ parameter vector length
+Return ``\hat{θ}`` parameter vector length
 """
 parameter_size(::Abstract_Map) = @assert(false,"To implement")
 
 @doc raw"""
 ```julia
-eval_map(m::Abstract_Map,X_hat::AbstractVector,θ::AbstractVector) -> X::AbstractVector
+eval_map(m::Abstract_Map,X_hat::AbstractVector,hat_θ::AbstractVector) -> X::AbstractVector
 ```
 
 Compute ``X=X(\hat{X})``.
 """
-eval_map(m::Abstract_Map,X_hat::AbstractVector,θ::AbstractVector) = @assert(false,"To implement")   
+eval_map(m::Abstract_Map,X_hat::AbstractVector,hat_θ::AbstractVector) = @assert(false,"To implement")   
