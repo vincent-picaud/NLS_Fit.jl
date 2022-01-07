b9bfd9d3beb3026cde5a6e3a4870533450468ac0
vincent-picaud <picaud.vincent@gmail.com>
2022-01-07 17:16:41 +0100

    Code: export Abstract_Model2Fit_Peak + some code doc

--- src/model2fit/peak/abstract_model2fit_peak.jl
+++ src/model2fit/peak/abstract_model2fit_peak.jl
@@ -1,1 +1,10 @@
+export Abstract_Model2Fit_Peak
+
+"""
+A specialization of [`Abstract_Model2Fit`](@ref) dedicated to peak types.
+
+TODO: in the future some extra methods are to be defined like 
+- peak_position
+- peak_area...
+"""
 abstract type Abstract_Model2Fit_Peak <: Abstract_Model2Fit end
